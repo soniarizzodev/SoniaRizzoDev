035ca300f2334206160f8d550164f108503fd072
Sonia Rizzo <soniarizzo.dev@gmail.com>
2019-11-20 18:36:31 +0100

    Procfile

--- runserver.py
+++ runserver.py
@@ -5,10 +5,13 @@ This script runs the SoniaRizzoDev application using a development server.
 from os import environ
 from webapp import app
 
-if __name__ == '__main__':
+def start():
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT)
+
+if __name__ == '__main__':
+    start()
