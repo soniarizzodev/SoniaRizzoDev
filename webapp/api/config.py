b17d849aa10c11c8739427a388534e27a4c11c21
soniarizzodev <soniarizzo.dev@gmail.com>
2020-03-12 21:26:06 +0100

    - making Todoist call more fail-proof

--- webapp/api/config.py
+++
@@ -1,1 +0,0 @@
-api_key="YOUR TODOIST API KEY"
--- webapp/api/srd_todoist.py
+++ webapp/api/srd_todoist.py
@@ -5,25 +5,25 @@ from webapp.api.Response import Response
 def get_tasks(*kwargs):
 
     api = todoist.api.TodoistAPI(get_api())
+
     api.sync()
 
-    # Get tech project from todoist
-    tech_project = next(project for project in api.state['projects'] if 'tech' in project['name'].lower())   
-
-    # Get all tasks belonging to tech project
-    tech_proj_id = tech_project['id']
-
     tasks_names = []
-    for task in api.state['items']:
-        if task['project_id'] == tech_proj_id and not task['checked'] and not task['in_history'] and not task['is_deleted']:
-            tasks_names.append(task['content'])
+
+    # Get tech project from todoist
+    tech_project = next((project for project in api.state['projects'] if 'tech' in project['name'].lower()), None)   
+
+    if tech_project is not None:
+        # Get all tasks belonging to tech project
+        tech_proj_id = tech_project['id']
+
+    
+        for task in api.state['items']:
+            if task['project_id'] == tech_proj_id and not task['checked'] and not task['in_history'] and not task['is_deleted']:
+                tasks_names.append(task['content'])
 
     response = Response(True)
 
     response.add("tasks", tasks_names)
 
     return response.compose()
-
-
-    
-
